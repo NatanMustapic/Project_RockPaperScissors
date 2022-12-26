834a2983163f9829e4b4fc5964d6aa64fc0a4221
Natan Mustapic <natan.mustapic95@gmail.com>
2022-12-26 14:14:55 +0100

    5 Wins for Victory MISSING

--- script.js
+++ script.js
@@ -9,45 +9,80 @@ function getComputerChoice() {
         return "scissors";
 }   
 
-function getPlayerChoice() {
-    let choice = prompt("Rock, Paper or Scissors buddy? ");
-
-    if (choice.toLowerCase()==="rock" || choice.toLowerCase()==="paper" || choice.toLowerCase()==="scissors") 
-        return choice.toLowerCase();
-    else
-        console.log("Incorrect selection my man!");
-}
-
 function playRound(playerSelection, computerSelection){
-    console.log("Your selection is " + playerSelection +", and my SMARTER selection is "+ computerSelection+".")
-    if (playerSelection === computerSelection)
-        console.log("Great minds do think alike... This demands a rematch!")
+    document.getElementById("output").innerHTML = "";
+    document.getElementById("output").innerHTML = "Your selection is " + playerSelection +", and my SMARTER selection is "+ computerSelection+".";
 
-    else if (playerSelection === "rock" & computerSelection === "paper")
-        console.log("Computers are the victorious ones! Prepare for the end.");
-    else if (playerSelection === "rock" & computerSelection === "scissors")
-        console.log("Even the superior brains ger outmatched sometimes... Don't get used on it!");
+    if (playerSelection === computerSelection){
+        document.getElementById("output").innerHTML +=" Great minds do think alike... This demands a rematch!";
+        TableWrite(playerSelection, computerSelection, "Draw");
+    }
+        
+
+    else if (playerSelection === "rock" & computerSelection === "paper"){
+        document.getElementById("output").innerHTML += " Computers are the victorious ones! Prepare for the end.";
+        TableWrite(playerSelection, computerSelection, "Computer");
+        WinnerwinnerChickenDinner("Computer");
+    }
+    else if (playerSelection === "rock" & computerSelection === "scissors"){
+        document.getElementById("output").innerHTML +=" Even the superior brains ger outmatched sometimes... Don't get used on it!";
+        TableWrite(playerSelection, computerSelection, "Human");
+        WinnerwinnerChickenDinner("Human");
+    }
     
-        else if (playerSelection === "paper" & computerSelection === "scissors")
-        console.log("Computers are the victorious ones! Prepare for the end.");
-    else if (playerSelection === "paper" & computerSelection === "rock")
-        console.log("Even the superior brains ger outmatched sometimes... Don't get used on it!");
+    else if (playerSelection === "paper" & computerSelection === "scissors"){
+        document.getElementById("output").innerHTML += " Computers are the victorious ones! Prepare for the end.";
+        TableWrite(playerSelection, computerSelection, "Computer");
+        WinnerwinnerChickenDinner("Computer");
+    }
+    else if (playerSelection === "paper" & computerSelection === "rock"){
+        document.getElementById("output").innerHTML += " Even the superior brains ger outmatched sometimes... Don't get used on it!";
+        TableWrite(playerSelection, computerSelection, "Human");
+        WinnerwinnerChickenDinner("Human");
+    }
 
-    else if (playerSelection === "scissors" & computerSelection === "rock")
-        console.log("Computers are the victorious ones! Prepare for the end.");
-    else if (playerSelection === "scissors" & computerSelection === "paper")
-        console.log("Even the superior brains ger outmatched sometimes... Don't get used on it!");
-}
-
-function game() {
-    let br = 1;
-    while (br<6){
-        console.log("ROUND NO. "+br+".")
-        playRound(getPlayerChoice(), getComputerChoice());
-        br+=1;
+    else if (playerSelection === "scissors" & computerSelection === "rock"){
+        document.getElementById("output").innerHTML += " Computers are the victorious ones! Prepare for the end.";
+        TableWrite(playerSelection, computerSelection, "Computer");
+        WinnerwinnerChickenDinner("Computer");
+    }
+    else if (playerSelection === "scissors" & computerSelection === "paper"){
+        document.getElementById("output").innerHTML +=" Even the superior brains ger outmatched sometimes... Don't get used on it!";
+        TableWrite(playerSelection, computerSelection, "Human");
+        WinnerwinnerChickenDinner("Human");
     }
 }
 
+function TableWrite(playerSelection, computerSelection, result){
+    const resultTable = document.getElementById("results");
 
-game();
+    var row = resultTable.insertRow(-1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
 
+    cell1.innerHTML = playerSelection;
+    cell2.innerHTML = computerSelection;
+    cell3.innerHTML = result;
+}
+
+function WinnerwinnerChickenDinner(result){
+    const ComputerScore = document.getElementById("compScore");
+    const HumanScore = document.getElementById("humanScore");
+
+    var computer = parseInt(ComputerScore.innerHTML);
+    var human = parseInt(HumanScore.innerHTML);
+
+    if (result === "Computer"){
+        computer +=1;
+        console.log(computer);
+        ComputerScore.innerHTML = computer;
+    }
+    else if (result === "Human"){
+        human +=1;
+        console.log(human);
+        HumanScore.innerHTML = human;
+    }
+    
+    //Provjera 5 dobitaka!!!
+}
